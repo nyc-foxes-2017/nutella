417181ac798b8bb949a27979ad70d3aee481d38c
Michael Benson <email": null>
2017-04-05 16:08:36 -0400

    split into components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SidebarView from './SidebarView';
-
+import Timeline from './Timeline'
 export default class App extends React.Component {
   render() {
     return (
@@ -10,7 +10,7 @@ export default class App extends React.Component {
         <nav> </nav>
         <div className="row">
         <SidebarView />
-
+        <Timeline />
         </div>
       </div>
     );
